3be2a960e563214e16031e5e0aaf91a01ad53839
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2023-01-10 17:48:59 -0500

    include testing

---
+++ test_check.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestCheck(TestCase):
+    def test_get_meeting_info1(self):
+        self.assertEqual(0,0)
+
+    def test_get_meeting_info2(self):
+        self.assertNotEqual(0,1)
