b475bee2b847d7a924ad2c678a6db995087755f5
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2023-01-10 17:50:57 -0500

    testing to-Dos

--- test_check.py
+++ test_check.py
@@ -1,5 +1,11 @@
 from unittest import TestCase
 
+"""
+To dos: 
+ [1] write a test case around print stuff (https://stackoverflow.com/questions/33767627/python-write-unittest-for-console-print) 
+ [2] write 4 more test cases
+ [3] have GitHub actions running on repo to automate testing when push
+"""
 
 class TestCheck(TestCase):
     def test_get_meeting_info1(self):
